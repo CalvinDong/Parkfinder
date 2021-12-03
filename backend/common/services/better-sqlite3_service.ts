14e054fa0ced3a4cdf1bf1a385d30a745a595c86
Calvin Dong <calvin.dong156@gmail.com>
2021-12-03 20:52:44 +1100

    - typescript changes

--- backend/common/services/better-sqlite3_service.ts
+++ backend/common/services/better-sqlite3_service.ts
@@ -1,21 +1,20 @@
 import Database from "better-sqlite3"
+import {Database as SqliteDatabase} from "better-sqlite3"
 
 export default new class SqliteService{
-  private db = new Database('parkfinder.db', { verbose: console.log });
-  /*constructor(){
-
-  }*/
+  //private db = new Database('parkfinder.db', { verbose: console.log });
+  private db: SqliteDatabase
+  
+  constructor(){
+    this.db = new Database('parkfinder.db', { verbose: console.log });
+  }
 
   public async ReadAll(){
     const readEverything = this.db.prepare(
-      "SELECT * FROM Base"
+      "SELECT * FROM Parks"
     );
     const result = await readEverything.all()
     return result
   }
 
 }
-
-
-
-//const Database = require('better-sqlite3');
