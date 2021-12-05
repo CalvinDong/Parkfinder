8dac7ec6dedc7ee00ceb2f0b98ddffc1dddde50e
Calvin Dong <calvin.dong156@gmail.com>
2021-12-05 18:59:57 +1100

    - added some more sqlite functions

--- backend/common/services/better-sqlite3_service.ts
+++ backend/common/services/better-sqlite3_service.ts
@@ -2,18 +2,26 @@ import Database from "better-sqlite3"
 import {Database as SqliteDatabase} from "better-sqlite3"
 
 export default new class SqliteService{
-  //private db = new Database('parkfinder.db', { verbose: console.log });
   private db: SqliteDatabase
   
   constructor(){
-    this.db = new Database('parkfinder.db', { verbose: console.log });
+    this.db = new Database('parkfinder.db', { verbose: console.log, readonly: true }); // Should make function that handles when db can't be found
   }
 
   public async ReadAll(){
+    const testStatement = "Parks, BBQ";
     const readEverything = this.db.prepare(
-      "SELECT * FROM Parks"
+      `SELECT * FROM ${testStatement}`
     );
     const result = await readEverything.all()
+    return result
+  }
+
+  public async ReadAllTableNames(){
+    const readEveryTable = this.db.prepare(
+      `SELECT name FROM sqlite_master WHERE type='table'`
+    );
+    const result = await readEveryTable.all()
     return result
   }
 
