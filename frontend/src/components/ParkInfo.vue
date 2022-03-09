265b3dc6e74eb3bf02308f4801a04ba2498b4969
CalvinDong <calvin.dong156@gmail.com>
2022-03-09 18:05:45 +1100

    - carousel works

--- frontend/src/components/ParkInfo.vue
+++ frontend/src/components/ParkInfo.vue
@@ -1,20 +1,28 @@
 <template>
-  <n-card v-bind:title="this.name" closable @close="handleClose"> 
-    <template #cover>
-      <img :src=img>
+  <n-card v-bind:title="this.name" closable @close="handleClose">
+    <template #cover> 
+      <vueper-slides fade: touchable="false" >
+        <vueper-slide v-for="(slide, i) in slides" :key="i" :image="slide.image" />
+      </vueper-slides>
     </template>
     {{this.info}} 
   </n-card>
 </template>
 
 <script>
-import { NCard } from 'naive-ui';
+import { NCard, NCarousel, NButton } from 'naive-ui';
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
 
 import axios from "axios";
 
 export default {
   components: {
-    NCard
+    NCarousel,
+    NCard,
+    NButton,
+    VueperSlides,
+    VueperSlide
   },
 
   props:["geoInfo"],
@@ -23,7 +31,7 @@ export default {
     return {
       name: null,
       info: null,
-      img: "https://firebasestorage.googleapis.com/v0/b/parkfinder-cd1be.appspot.com/o/IMG_5080-3.jpg?alt=media&token=89ee23ba-bf5c-4bc8-877c-5ef99f084678"
+      slides: null,
     }
   },
 
@@ -39,6 +47,12 @@ export default {
       this.name = info.data.name;
       //this.name = infoGeo.name;
       this.info = info.data.description
+      let imgRes = await axios.post(`http://localhost:4000/getParkImages`,{
+        name: info.data.name,
+        type: info.data.type,
+        id: info.data.id
+      })
+      this.slides = imgRes.data;
     },
 
     handleClose(){
@@ -51,6 +65,20 @@ export default {
 <style>
   .n-card {
     min-width: 300px;
+  }
+
+  .carousel-img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  .vueeep{
+    height: 500px
+  }
+
+  .vues{
+    height: 500px
   }
 
 </style>
